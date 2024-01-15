bc323e307a3b54202fbe4d3ef709f1859298ab8c
aniket3765 <ingawale3765@gmail.com>
2024-01-15 19:26:48 +0530

    using forwardRef to use ref in custom component Input

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState , useReducer, useContext} from 'react';
+import React, { useState , useReducer, useContext, useRef } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -37,6 +37,9 @@ const collegeReducer = (state, action) => {
 }
 const Login = (props) => {
   const authCtx = useContext(AuthContext);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const collegeInputRef = useRef();
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {value:'', isValid:null});
@@ -67,13 +70,21 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value, collegeState.value);
+    if(formIsValid) authCtx.onLogin(emailState.value, passwordState.value, collegeState.value);
+    else if(!emailState.isValid){
+      emailInputRef.current.focus();
+    }
+   else  if(!passwordState.isValid){ 
+      passwordInputRef.current.focus();
+    }
+    else if(!collegeState) collegeInputRef.current.focus();
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input 
+        ref={emailInputRef}
         id="email" 
         label="E-mail" 
         type="email"
@@ -83,6 +94,7 @@ const Login = (props) => {
         onBlur={validateEmailHandler}
         ></Input>
        <Input 
+       ref={passwordInputRef}
         id="password" 
         label="Password" 
         type="password"
@@ -91,7 +103,8 @@ const Login = (props) => {
         onChange={passwordChangeHandler}
         onBlur={validatePasswordHandler}
         ></Input>
-<Input 
+<Input  
+        ref={collegeInputRef}
         id="college" 
         label="College" 
         type="text"
@@ -102,7 +115,7 @@ const Login = (props) => {
         ></Input>
 
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
